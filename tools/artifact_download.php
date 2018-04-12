a40b7838af87ea009742cded341b38db4ad3e23e
Vitaliy Honcharenko <vgoncharenko@magento.com>
2018-04-12 10:17:16 +0300

    init

---
+++ tools/artifact_download.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vgoncharenko
+ * Date: 4/12/18
+ * Time: 9:33 AM
+ */
+
+define('BP', __DIR__);
+include BP . '/../bootstrap.php';
+define('SSH_CONFIG', '~/.ssh/config');
+date_default_timezone_set('UTC');
+
+$config = new \App\Config(BP . '/../resources/config.json');
+
+foreach ($config->getInstances() as $item) {
+    if (!$item['include']) {
+        continue;
+    }
+
+    foreach ($item['profiles'] as $profile) {
+        foreach ($profile['measurements'] as $measurement) {
+            $sourceFile = " {$item['jenkins']}:/var/lib/jenkins/jobs/{$item['jenkins_folder']}/builds/{$measurement['build_id']}/archive/{$measurement['src']} ";
+            $destinationFile = BP . "/../temp/{$item['type']}/{$profile['name']}/{$measurement['type']}/{$measurement['src']}";
+            `rm -rf $destinationFile`;
+            $destinationDirName = dirname($destinationFile);
+            if (!file_exists($destinationDirName)) {
+                `mkdir -p $destinationDirName`;
+            }
+            $cmd = "scp -r -F " . SSH_CONFIG . $sourceFile . " ". $destinationFile;
+            echo $cmd . "\n";
+            `$cmd`;
+            echo "COMPLETE \n";
+        }
+    }
+}
